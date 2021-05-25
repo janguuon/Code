39d9b853b6022d4a4e80e825f5c7bfbb1a28a171
Janguuon <Janguuon@DESKTOP-0UF7LGF>
2021-05-26 00:25:32 +0900

    onetwofourcountry.

---
+++ OneTwoFourCountry/OneTwoFourCountry/OneTwoFourCountry.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+std::string solution(int n)
+{
+    std::string answer;
+
+    int num = 0;
+
+    while (0 < n)
+    {
+        num = n % 3;
+        n = n / 3;
+
+        if (0 == num)
+        {
+            n -= 1;
+            num = 4;
+        }
+
+        answer = std::to_string(num) + answer;
+    }
+
+
+    return answer;
+}
+
+int main()
+{
+
+    std::cout << solution(15);
+
+    return 0;
+}
+

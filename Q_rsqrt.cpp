39ce096bb262713e2fd081251b11810d22114c15
janguuon <53895461+janguuon@users.noreply.github.com>
2024-09-04 16:31:22 +0900

    Add files via upload

---
+++ Q_rsqrt.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+float Q_rsqrt(float number)
+{
+    long i;
+    float x2, y;
+    const float threehalfs = 1.5f;
+
+    x2 = number * 0.5f;
+    y = number;
+    i = *(long*)&y;
+    i = 0x5f3759df - (i >> 1);
+    y = *(float*)&i;
+    y = y * (threehalfs - (x2 * y * y));
+
+    return y;
+}
+
+int main()
+{
+    Q_rsqrt(0.24897f);
+
+    std::cout << "Hello World!\n";
+}
